120c01a0ce9349601d7bf0ceeb878c2d55b558fe
Kristian <krisgeorgiev2002@gmail.com>
2025-06-07 16:54:37 +0300

    Implement order creation form and success page

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
+use App\Form\OrderTypeForm;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class OrderController extends AbstractController
 {
-    #[Route('/order', name: 'app_order')]
-    public function index(): Response
+    #[Route('/order', name: 'order_create')]
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
-        return $this->render('order/index.html.twig', [
-            'controller_name' => 'OrderController',
+        $order = new Order();
+        $order->setCreatedAt(new \DateTimeImmutable());
+
+        $form = $this->createForm(OrderTypeForm::class, $order);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em->persist($order);
+            $em->flush();
+
+        return $this->render('order/success.html.twig', [
+            'order'=> $order,
         ]);
     }
+    return $this->render('order/index.html.twig', [
+        'form' => $form->createView(),
+    ]);
+}
 }
--- src/Form/OrderTypeForm.php
+++ src/Form/OrderTypeForm.php
@@ -4,21 +4,48 @@ namespace App\Form;
 
 use App\Entity\Order;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class OrderTypeForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('clientName')
-            ->add('email')
-            ->add('size')
-            ->add('ingredients')
-            ->add('comment')
-            ->add('createdAt', null, [
-                'widget' => 'single_text',
+            ->add('clientName', TextType::class, [
+                'label' => 'Client Name',
+        ])
+            ->add('email', EmailType::class, ['label' => 'Email'])
+            ->add('size', ChoiceType::class, [
+                'choices' => [
+                    'Small' => 'S',
+                    'Medium' => 'M',
+                    'Large' => 'L',
+                ],
+                'label' => 'Pizza size',
+            ])
+            ->add('ingredients', ChoiceType::class, [
+                'choices' => [
+                    'Pepperoni' => 'pepperoni',
+                    'Mushrooms' => 'mushrooms',
+                    'Onions' => 'onions',
+                    'Sausage' => 'sausage',
+                    'Bacon' => 'bacon',
+                    'Black olives' => 'black_olives',
+                    'Green peppers' => 'green_peppers',
+                    'Pineapple' => 'pineapple',
+                ],
+                'label' => 'Ingredients',
+                'multiple' => true,
+                'expanded' => true,
+            ])
+            ->add('comment', TextType::class, [
+                'label' => 'Comment',
+                'required' => false,
             ])
         ;
     }
